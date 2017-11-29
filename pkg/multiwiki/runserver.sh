d2448d97104948f553d1ae86d76fc43acf3f38ab
Benjamin Travis Summers <benjamin@bsummers.me>
2017-11-29 15:49:15 +0000

    MVP tarball uploading.

--- pkg/multiwiki/runserver.sh
+++ pkg/multiwiki/runserver.sh
@@ -1,7 +1,7 @@
 set -e
 set -x
 
-hspkgs='classy-prelude lens text scotty'
+hspkgs='classy-prelude lens text scotty wai-middleware-static'
 
 nix-shell                                                                     \
   -p "haskell.packages.ghc802.ghcWithPackages (pkgs: with pkgs; [ $hspkgs ])" \
--- pkg/multiwiki/takeout-upload.hs
+++ pkg/multiwiki/takeout-upload.hs
@@ -10,9 +10,56 @@
 
 import ClassyPrelude
 import Control.Lens    as Lens
-import Web.Scotty
+import Web.Scotty      as S
 
-main :: IO ()
+import System.FilePath ((</>))
+
+import qualified Network.Wai.Middleware.RequestLogger as Wai
+import qualified Network.Wai.Middleware.Static        as Wai
+import qualified Network.Wai.Parse                    as Wai
+import qualified System.Directory                     as Sys
+import qualified Data.ByteString.Lazy                 as B
+import qualified Data.ByteString.Char8                as BS
+import qualified System.Process                       as Sys
+
+io = liftIO
+
+mkDir ∷ FilePath → IO FilePath
+mkDir path = do
+  ex ← Sys.doesDirectoryExist path
+  unless ex (Sys.createDirectoryIfMissing True path)
+  pure path
+
+main ∷ IO ()
 main = scotty 3000 $ do
   get "/" $ do
-    html "<h1>Hello World</h1>"
+    html $ mconcat
+      [ "<!DOCTYPE html>"
+      , "<html>"
+      , "<body>"
+      , ""
+      , "<form action=\"./upload\" method=\"post\" enctype=\"multipart/form-data\">"
+      , "    Select tarball to upload:"
+      , "    <input type=\"file\" name=\"fileToUpload\" id=\"fileToUpload\">"
+      , "    <input type=\"submit\" value=\"Upload Tarball\" name=\"submit\">"
+      , "</form>"
+      , ""
+      , "</body>"
+      , "</html>"
+      ]
+
+  post "/upload" $ do
+    fs <- files
+
+    let fs' = do (fieldName, fi) <- fs
+                 pure (fieldName, BS.unpack (Wai.fileName fi), Wai.fileContent fi)
+
+    io $ mkDir "./takeout-tarballs"
+
+    io $ do
+      for_ fs' $ \(_, fn, fc) → do
+        let tarballFile = "./takeout-tarballs/" </> fn
+        B.writeFile tarballFile fc
+        Sys.callProcess "./mkwiki.sh" ["./takeout-tarballs", tarballFile]
+
+    html "<h1>File uploaded and processed</h1>"
